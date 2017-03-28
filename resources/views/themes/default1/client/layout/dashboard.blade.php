635ca52e43d8b732f65e60ad9cf3f2e0194d43fc
Tommy Ho <tommy.ho@allbrightnet.com>
2017-03-29 02:12:16 +0800

    live helper chat support

--- resources/views/themes/default1/client/layout/dashboard.blade.php
+++ resources/views/themes/default1/client/layout/dashboard.blade.php
@@ -411,8 +411,18 @@
             }
             }
         </script>
-        <script>
-</script>
+        <script type="text/javascript">
+            var lang = '/chn';
+            var LHCChatOptions = {};
+            LHCChatOptions.opt = {widget_height:340,widget_width:300,popup_height:520,popup_width:500};
+            (function() {
+            var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+            var referrer = (document.referrer) ? encodeURIComponent(document.referrer.substr(document.referrer.indexOf('://')+1)) : '';
+            var location  = (document.location) ? encodeURIComponent(window.location.href.substring(window.location.protocol.length)) : '';
+            po.src = '//livehelperchat.allbrightnet.com'+lang+'/chat/getstatus/(click)/internal/(position)/bottom_right/(ma)/br/(check_operator_messages)/true/(top)/350/(units)/pixels/(leaveamessage)/true?r='+            referrer+'&l='+location;
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+            })();
+        </script>        
 <?php Event::fire('show.calendar.script', array()); ?>
 <?php Event::fire('load-calendar-scripts', array()); ?>
         @yield('FooterInclude')
