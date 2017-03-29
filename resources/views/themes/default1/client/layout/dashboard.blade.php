a37521f33ead36d60e02fe561b624e25ca6996ea
Tommy Ho <tommy.ho@allbrightnet.com>
2017-03-29 09:29:59 +0800

    Hide Live Chat Helper when no admin online

--- resources/views/themes/default1/client/layout/dashboard.blade.php
+++ resources/views/themes/default1/client/layout/dashboard.blade.php
@@ -419,7 +419,7 @@
             var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
             var referrer = (document.referrer) ? encodeURIComponent(document.referrer.substr(document.referrer.indexOf('://')+1)) : '';
             var location  = (document.location) ? encodeURIComponent(window.location.href.substring(window.location.protocol.length)) : '';
-            po.src = '//livehelperchat.allbrightnet.com'+lang+'/chat/getstatus/(click)/internal/(position)/bottom_right/(ma)/br/(check_operator_messages)/true/(top)/350/(units)/pixels/(leaveamessage)/true?r='+            referrer+'&l='+location;
+            po.src = '//livehelperchat.allbrightnet.com'+lang+'/chat/getstatus/(click)/internal/(position)/bottom_right/(ma)/br/(hide_offline)/true/(check_operator_messages)/true/(top)/350/(units)/pixels/(leaveamessage)/true?r='+ referrer+'&l='+location;
             var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
             })();
         </script>        
