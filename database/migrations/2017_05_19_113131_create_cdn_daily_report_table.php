38036872f6eb62b6968be639dd115e1f55da60be
Tommy Ho <tommy.ho@allbrightnet.com>
2017-05-22 12:49:16 +0800

    revamp cdn daily report table

--- database/migrations/2017_05_19_113131_create_cdn_daily_report_table.php
+++ database/migrations/2017_05_19_113131_create_cdn_daily_report_table.php
@@ -13,18 +13,17 @@ class CreateCdnDailyReportTable extends Migration
     public function up()
     {
         Schema::create('cdn_daily_report', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('resource_id')->unsigned();
             $table->date('report_date');
-            $table->integer('http_byte')->unsigned();
-            $table->integer('https_byte')->unsigned();
-            $table->integer('total_byte')->unsigned();
-            $table->integer('total_req')->unsigned();
+            $table->integer('resource_id')->unsigned();
+            $table->bigInteger('http_byte')->unsigned();
+            $table->bigInteger('https_byte')->unsigned();
+            $table->bigInteger('total_byte')->unsigned();
+            $table->bigInteger('total_req')->unsigned();
             $table->timestamps();
         });
         Schema::table('cdn_daily_report', function (Blueprint $table) {
             //$table->foreign('resource_id', 'cdn_daily_report_resource_ibfk_1')->references('id')->on('cdn_resources')->onUpdate('NO ACTION')->onDelete('CASCADE');
-            $table->unique(['resource_id', 'report_date'], 'cdn_daily_report_id_date_uk_1');
+            $table->primary(['report_date', 'resource_id']);
         });
     }
 
