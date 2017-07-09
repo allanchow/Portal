6df5894871705959ead5868e30692406abd5740f
Tommy Ho <tommy.ho@allbrightnet.com>
2017-07-09 18:32:45 +0800

    change log format

--- vendor/barryvdh/laravel-debugbar/src/DataCollector/LogsCollector.php
+++ vendor/barryvdh/laravel-debugbar/src/DataCollector/LogsCollector.php
@@ -25,13 +25,13 @@ class LogsCollector extends MessagesCollector
     public function getLogsFile()
     {
         // default daily rotating logs (Laravel 5.0)
-        $path = storage_path() . '/logs/laravel-' . date('Y-m-d') . '.log';
-        
+        $path = storage_path() . '/logs/laravel-' . date('Y-m-d') . '-' . get_current_user() . '.log';
+
         // single file logs
         if (!file_exists($path)) {
-            $path = storage_path() . '/logs/laravel.log';
+            $path = storage_path() . '/logs/laravel' . get_current_user() . '.log';
         }
-        
+
         return $path;
     }
 
